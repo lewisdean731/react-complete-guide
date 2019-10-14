d8c4ae594c320ce3b607a0ac3c0644db0567b875
Lewis Dean <ldean@ukcloud.com>
2019-10-14 11:45:55 +0100

    S4/L56 Lists and State

--- src/App.js
+++ src/App.js
@@ -12,12 +12,10 @@ class App extends Component {
     showPersons: true
   }
 
-  switchNameHandler = (newName) => {
-    this.setState( {
-        persons: [
-        {name: newName, age: 21}
-      ]
-    } )
+  deletePersonHandler = (personIndex) => {
+    const persons=this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
   }
 
   nameChangedHandler = (event) => {
@@ -49,8 +47,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person
+              click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}/>
           })}
