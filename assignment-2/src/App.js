6f68cd490d8b5626af9ac1188afa7fbee667b936
Lewis Dean <ldean@ukcloud.com>
2019-10-18 14:51:02 +0100

    Remove ignored git stuff

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -1,88 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import ValidationComponent from './ValidationComponent/ValidationComponent'
 
 class App extends Component {
   state = {
-    persons: [
-      {id: 'b64-1', name: 'Lewis', age: 20},
-      {id: 'b64-2', name: 'John', age: 22},
-      {id: 'b64-3', name: 'April', age: 26}
-    ],
-    showPersons: true
+    text: 'test',
+    textLengthStatus: 'Too short!'
   }
 
-  deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons
-    const persons=[...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons});
-  }
+  textChangeHandler = ( event ) => {
+    this.setState( {text: event.target.value} );
 
-  nameChangedHandler = ( event, id ) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
-    });
-
-    const person = {
-      ...this.state.persons[personIndex]
-    };
-
-    // const person = Object.assign({}, this.state.persons[personIndex]);
-
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-
-    this.setState( {persons: persons} );
-  }
-
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState( {
-        showPersons: !doesShow
-    } )
+    if( this.state.text.length < 5 ) {
+      this.setState( {textLengthStatus: 'Too short!'} );
+    }
+    else if( this.state.text.length > 5 ) {
+      this.setState( {textLengthStatus: 'Too long!'} );
+    }
+    else {
+      this.setState( {textLengthStatus: 'Just right!'} );
+    }
   }
 
   render() {
-    const style = {
-      backgroundColor: '#EEEEEE',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '1%',
-      cursor: 'pointer'
-    }
 
-    let persons = null;
-
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              changed={(event) => this.nameChangedHandler(event, person.id)}
-              name={person.name}
-              age={person.age}
-              key={person.id} />
-          })}
-        </div>
-      );
-    }
-    
     return (
       <div className="App">
-      <button
-        style={style}
-        onClick={() => this.switchNameHandler('Lucy')}>Switch Name
-      </button>
-      <button
-        style={style}
-        onClick={() => this.togglePersonsHandler()}>Toggle Persons
-      </button>
-          <h1>Hi, I'm a React App</h1>
-          {persons}
+        <input type="text" onChange={this.textChangeHandler}/>
+        <ValidationComponent
+          text={this.state.text}>
+        </ValidationComponent>
       </div>
     );
   }
