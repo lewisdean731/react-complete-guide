d35c5605b1bc6bf3c18686afdcea3d36e06b343a
Lewis Dean <ldean@ukcloud.com>
2019-10-18 14:51:02 +0100

    Remove ignored git stuff

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import ValidationComponent from './ValidationComponent/ValidationComponent'
-import CharComponent from './CharComponent/CharComponent'
 
 class App extends Component {
   state = {
     text: 'test',
-    textLengthStatus: 'Too short!',
-    textAsCharArray: 'test'
+    textLengthStatus: 'Too short!'
   }
 
   textChangeHandler = ( event ) => {
@@ -22,36 +20,16 @@ class App extends Component {
     else {
       this.setState( {textLengthStatus: 'Just right!'} );
     }
-
-    this.setState( {textAsCharArray: this.state.text.split('')})
-  }
-
-  deleteCharboxHandler = ( index ) => {
-    const textAsCharArray = this.state.text.split('');
-    textAsCharArray.splice(index, 1);
-    const newText = textAsCharArray.join('');
-    this.setState({text: newText});
   }
 
   render() {
-
-    const charList = this.state.text.split('').map((char, index) => {
-      return <CharComponent
-        char={char}
-        key={index}
-        clicked={() => this.deleteCharboxHandler(index)} />;
-    });
 
     return (
       <div className="App">
         <input type="text" onChange={this.textChangeHandler}/>
         <ValidationComponent
-          text={this.state.text}
-          textLengthStatus={this.state.textLengthStatus}>
+          text={this.state.text}>
         </ValidationComponent>
-        <div>
-          {charList}
-        </div>
       </div>
     );
   }
