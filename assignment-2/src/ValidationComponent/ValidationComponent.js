6da0564759577c01f9beb4955f01889cec62cab9
Lewis Dean <ldean@ukcloud.com>
2019-10-22 16:29:10 +0100

    Remove merge conflict leftovers

--- assignment-2/src/ValidationComponent/ValidationComponent.js
+++ assignment-2/src/ValidationComponent/ValidationComponent.js
@@ -4,11 +4,7 @@ import './ValidationComponent.css'
 
 const validationComponent = (props) => {
   return (
-<<<<<<< HEAD
     <div className="ValidationComponent">
-=======
-    <div className="Person">
->>>>>>> Add ValidationComponent
       <p>{props.text}</p>
       <hr />
       <p>{props.textLengthStatus}</p>
