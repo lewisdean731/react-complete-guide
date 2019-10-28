93fc7899f087123f0e3178c9f3965589c7402ea8
Lewis Dean <ldean@ukcloud.com>
2019-10-28 12:42:01 +0000

    S6/L81

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.module.css';
-import Person from './Person/Person'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Person from './Person/Person';
 
 class App extends Component {
   state = {
@@ -54,12 +55,13 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              changed={(event) => this.nameChangedHandler(event, person.id)}
-              name={person.name}
-              age={person.age}
-              key={person.id} />
+            return <ErrorBoundary key={person.id}>
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+                name={person.name}
+                age={person.age}/>
+            </ErrorBoundary>
           })}
         </div>
       );
