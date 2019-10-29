da88d09b31c0877b7e37c1c2163866ee2f14800b
Lewis Dean <ldean@ukcloud.com>
2019-10-29 15:10:18 +0000

    S7/L90

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -4,6 +4,11 @@ import Cockpit from '../components/Cockpit/Cockpit';
 import Persons from '../components/Persons/Persons';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[app.js] constructor');
+  }
+
   state = {
     persons: [
       {id: 'b64-1', name: 'Lewis', age: 20},
@@ -11,6 +16,19 @@ class App extends Component {
       {id: 'b64-3', name: 'April', age: 26}
     ],
     showPersons: true
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentWillMount() {
+    console.log('[App.js] componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
   }
 
   deletePersonHandler = (personIndex) => {
@@ -45,6 +63,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render')
     let persons = null;
 
     if (this.state.showPersons) {
