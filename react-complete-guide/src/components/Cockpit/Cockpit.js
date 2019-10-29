72fe7e21d6da156df43bf400943085ab1e3cc129
Lewis Dean <ldean@ukcloud.com>
2019-10-29 15:43:58 +0000

    S7/L94

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -4,7 +4,11 @@ import classes from './Cockpit.module.css';
 const cockpit = (props) => {
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
-  });
+    // ...http request
+    setTimeout(() => {
+      alert('Saved data to cloud!');
+    }, 1000);
+  }, []);
 
   const assignedClasses = [];
 
