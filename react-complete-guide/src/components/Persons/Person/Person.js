183d3803d34d8ca31703f4500713de1fd39d8805
Lewis Dean <ldean@ukcloud.com>
2019-10-30 13:57:05 +0000

    S7/L102
    Note: removes APP Css class

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import classes from './Person.module.css'
+import Aux from '../../../hoc/Aux'
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
-    return (
-      <div className={classes.Person}>
+    return(
+      <Aux>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age}!</p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name}/>
-      </div>
+      </Aux>
     );
   }
 }
