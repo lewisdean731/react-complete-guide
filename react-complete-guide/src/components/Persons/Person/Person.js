e26dbf2dc4f522ac80ae4410a75d1cec6a1335f1
Lewis Dean <ldean@ukcloud.com>
2019-10-30 14:41:43 +0000

    S7/L110

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -6,6 +6,14 @@ import PropTypes from 'prop-types';
 
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
   render() {
     console.log('[Person.js] rendering...');
     return(
@@ -13,6 +21,8 @@ class Person extends Component {
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age}!</p>
         <p>{this.props.children}</p>
         <input
+          // ref={(inputEl) => {this.inputElement = inputEl}}
+          ref={this.inputElementRef}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
