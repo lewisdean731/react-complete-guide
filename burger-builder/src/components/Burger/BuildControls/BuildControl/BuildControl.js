417ce7696e56c9098c43d4f0982527919d179d43
Lewis Dean <ldean@ukcloud.com>
2019-11-13 11:38:00 +0000

    S8/L163 Connecting state to build controls

--- burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,13 @@ import classes from './BuildControl.module.css';
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>Remove</button>
-        <button className={classes.More}>Add</button>
+        <button
+            className={classes.Less}
+        >Remove</button>
+        <button
+            className={classes.More}
+            onClick={props.ingredientAdded}
+        >Add</button>
     </div>
 );
 
