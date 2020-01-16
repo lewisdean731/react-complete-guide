7301e8fe8eaa81388651f260ac9d093bd44d2270
Lewis Dean <ldean@ukcloud.com>
2020-01-16 10:18:08 +0000

    S8/L176 Create a resoponsive side drawer

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 const layout = ( props ) => (
     <Aux>
-        <Toolbar></Toolbar>
+        <Toolbar />
+        <SideDrawer />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
