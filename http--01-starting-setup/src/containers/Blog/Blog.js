6b44a3c49b21e6807d5fab48eea13536ecf402fc
Lewis Dean <ldean@ukcloud.com>
2020-01-23 15:15:27 +0000

    S9/L193 render fetched data to screen

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -7,20 +7,29 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state={
+        posts: []
+    }
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log(response)
+                this.setState({posts: response.data});
+                // Console.log(reposonse);
             });
     }
 
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post
+                key={post.id}
+                title={post.title}
+                body={post.body}>
+            </Post>
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
