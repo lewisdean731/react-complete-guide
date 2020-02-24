ec56180b47abafcd47bd3c8fea6466ccf442abd4
Lewis Dean <ldean@ukcloud.com>
2020-02-24 15:11:06 +0000

    S9/L200 adding interceptors to execute code globally

--- http--01-starting-setup/src/index.js
+++ http--01-starting-setup/src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    console.log(request)
+    return request;
+}, error => {
+    console.log(error);
+});
+
+axios.interceptors.request.use(response => {
+    console.log(response)
+    return response;
+}, error => {
+    console.log(error);
+});
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
